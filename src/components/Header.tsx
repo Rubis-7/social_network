7c8d1ed2404551aea396c3806bb0308cc5595c83
Rubis-7 <zxc.zxz@yandex.ru>
2022-03-31 21:17:02 +0300

    social_network

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Header() {
+    return (
+        <header className='header'>
+            <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIGfafD_BE-xf3kib2jqS_DxgW5yD8pmEqgQ&usqp=CAU"
+                alt=""/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+function Profile() {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://www.shkolazhizni.ru/img/content/i187/187867_or.jpg" alt=""/>
+            </div>
+            <div>
+                ava+description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New posts
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
